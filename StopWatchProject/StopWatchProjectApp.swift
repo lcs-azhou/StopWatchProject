126ec054dcde99dd9579c6fc7454eb9ed4630636
Ansheng Zhou <azhou@lcs.on.ca>
2023-11-02 08:46:42 -0400

    Initial Commit

---
+++ StopWatchProject/StopWatchProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchProjectApp.swift
+//  StopWatchProject
+//
+//  Created by Ansheng Zhou on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
